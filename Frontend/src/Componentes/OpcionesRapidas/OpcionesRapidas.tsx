be7be0f3c3225b9851e8f4ffa8f027b7748df4ce
Matias Alarcon <141800043+MatiasAlarconArias@users.noreply.github.com>
2025-08-27 21:29:19 -0400

    cree botones solo estetico de crear

---
+++ Frontend/src/Componentes/OpcionesRapidas/OpcionesRapidas.tsx
@@ -0,0 +1,20 @@
+import './OpcionesRapidas.css';
+
+export default function OpcionesRapidas() {
+  return (
+    <>
+      <div className="Contenedor">
+        <div className="Container">
+          <h1>Opciones rapidas</h1>
+          <div className="cotenendor-botones">
+            <button>Crear Cuenta</button>
+            <button>Crear transaccion</button>
+            <button>Crear presupuesto</button>
+            <button>Crear meta</button>
+            <button>Crear categoria</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import NavBar from '../Componentes/navBar/navBar';
 import Carts from '../Componentes/Carts/Carts';
+import OpcionesRapidas from '../Componentes/OpcionesRapidas/OpcionesRapidas';
 
 export default function Dashboard() {
   return (
@@ -11,6 +12,9 @@ export default function Dashboard() {
         <div className="Container">
           <Carts />
         </div>
+      </section>
+      <section>
+        <OpcionesRapidas />
       </section>
     </>
   );
